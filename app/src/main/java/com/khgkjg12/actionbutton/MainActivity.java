eb854cddd20b171cb14b78fd106c1a7a85bbdac8
Kang Hyungu <khgkjg12@naver.com>
2018-09-10 16:44:11 +0900

    init commit

---
+++ app/src/main/java/com/khgkjg12/actionbutton/MainActivity.java
@@ -0,0 +1,13 @@
+package com.khgkjg12.actionbutton;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

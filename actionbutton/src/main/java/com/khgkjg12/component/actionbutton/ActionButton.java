cdb65ea306f0d8d43bae53123337fd08d6f9515c
Kang Hyungu <khgkjg12@naver.com>
2018-09-10 22:41:31 +0900

    일단 완성, press 시 어두워지는 효과, setClickable(false) 시에 투명해지고 동시에 progressbar 띄우기

--- actionbutton/src/main/java/com/khgkjg12/component/actionbutton/ActionButton.java
+++ actionbutton/src/main/java/com/khgkjg12/component/actionbutton/ActionButton.java
@@ -4,21 +4,30 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 /**
  * TODO: document your custom view class.
  */
 public class ActionButton extends Button {
 
+    ProgressBar mProgressBar;
+    int mProgressBarStyle;
+
     public ActionButton(Context context) {
         super(context);
+
         init(null, 0);
     }
 
@@ -31,18 +40,63 @@ public class ActionButton extends Button {
         super(context, attrs, defStyle);
         init(attrs, defStyle);
     }
-
     private void init(AttributeSet attrs, int defStyle) {
+        // Load attributes
+        final TypedArray a = getContext().obtainStyledAttributes(
+                attrs, R.styleable.ActionButton, defStyle, 0);
 
+        mProgressBarStyle = a.getResourceId(
+                R.styleable.ActionButton_progressBarStyle, 0);
+
+        a.recycle();
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction() == MotionEvent.ACTION_DOWN){
-            setAlpha(0.5f);
-        }else if(event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL){
-            setAlpha(1.0f);
+        boolean isHandled = super.onTouchEvent(event);
+        if(isHandled){
+            if(event.getAction() == MotionEvent.ACTION_DOWN){
+                getBackground().setColorFilter(Color.argb(100,0,0,0), PorterDuff.Mode.DARKEN);
+                //setAlpha(0.5f);
+            }else if(event.getAction() == MotionEvent.ACTION_UP || event.getAction() == MotionEvent.ACTION_CANCEL){
+                getBackground().clearColorFilter();
+                //setAlpha(1.0f);
+            }else if(event.getAction() == MotionEvent.ACTION_MOVE){
+                if(event.getX()<0 ||event.getX() > getWidth() || event.getY() <0 ||event.getY()>getHeight()){
+                    getBackground().clearColorFilter();
+                    //setAlpha(1.0f);
+                }
+            }
         }
-        return super.onTouchEvent(event);
+        return isHandled;
+    }
+
+    @Override
+    public void setClickable(boolean clickable) {
+        setAlpha(clickable?1.0f:0.6f);
+        ViewGroup parent = (ViewGroup)getParent();
+        if(parent != null) {
+            if(clickable){
+                if(mProgressBar!=null) {
+                    parent.removeView(mProgressBar);
+                    mProgressBar = null;
+                }
+            }else{
+                if(mProgressBar==null) {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && mProgressBarStyle!=0) {
+                        mProgressBar = new ProgressBar(getContext(), null, 0, mProgressBarStyle);
+                    }else{
+                        mProgressBar = new ProgressBar(getContext());
+                    }
+                    mProgressBar.setLayoutParams(new ViewGroup.LayoutParams(getWidth(), getHeight()));
+                    parent.addView(mProgressBar);
+                }
+            }
+        }
+        super.setClickable(clickable);
+    }
+
+    public void setProgressBarStyle(int style){
+        mProgressBarStyle = style;
     }
 }
--- app/src/main/java/com/khgkjg12/actionbutton/MainActivity.java
+++ app/src/main/java/com/khgkjg12/actionbutton/MainActivity.java
@@ -2,6 +2,9 @@ package com.khgkjg12.actionbutton;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.khgkjg12.component.actionbutton.ActionButton;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final ActionButton actionButton = findViewById(R.id.action_button);
+        actionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                actionButton.setText(R.string.wait);
+                actionButton.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        actionButton.setText(R.string.click);
+                        actionButton.setClickable(true);
+                    }
+                },3000);
+                v.setClickable(false);
+            }
+        });
     }
 }
